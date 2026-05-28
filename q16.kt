a5ff232ee31e9e4129cf8f6a2a00a94430855bd7
Asish Kumar Behera <cse.23bcsb66@silicon.ac.in>
2026-05-28 15:24:01 +0530

    Add temperature conversion between Celsius and Fahrenheit

---
+++ q16.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val c = 38
+	print("Temperature in Celsius: $c \n")
+    val fahrenheit = (c * 9 / 5) + 32
+    println("Fahrenheit = $fahrenheit \n")
+    val f = 100
+	print("Temperature in Fahrenheit: $f \n")
+    val cel = (f - 32) * 5 / 9
+    println("Celsius = $cel")
+}
