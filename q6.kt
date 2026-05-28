688d5401593dffbe4e9e9b1b2fd186eef819e48c
Asish Kumar Behera <cse.23bcsb66@silicon.ac.in>
2026-05-28 14:47:02 +0530

    Factorial calculation

--- q6.kt
+++ q6.kt
@@ -3,7 +3,7 @@ fun main() {
     val num = 6
     var factorial = 1
     for (i in 1..num) {
-        factorial *= i
+        factorial = factorial*i
     }
     println("Factorial = $factorial")
 }
